07f5671ccc22bd6a1af3b012e8ae2fd52b6e7063
Sergey Klyusov <donot@email.me>
2024-10-02 17:01:37 +0500

    backing up

--- build_install.sh
+++ build_install.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-go build && sudo install sms_daemon /usr/local/sbin/ && sudo systemctl restart sms_daemon && sleep 1 && sudo systemctl --no-pager status sms_daemon && echo "Check later:" && echo "sudo systemctl --no-pager status sms_daemon"
+cd build
+go build ../ && sudo install sms_daemon /usr/local/sbin/ && sudo systemctl restart sms_daemon && sleep 1 && sudo systemctl --no-pager status sms_daemon && echo "Check later:" && echo "sudo systemctl --no-pager status sms_daemon"
